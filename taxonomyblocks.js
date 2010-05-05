98769a41b61ce4b4b35af8dc629b3b92cc4d4e77
Aaron <aaron_hawkins@128806.no-reply.drupal.org>
2010-05-05 08:36:35 +0000

    Minor alteration

--- taxonomyblocks.js
+++ taxonomyblocks.js
@@ -1,6 +1,9 @@
 // $Id$
 $(document).ready(function(){
   var vocab_over = function() {
+    var link_width = $(this).children('.vocab_edit_link').width() + 15;
+    
+    $(this).children('.vocab_edit_link').css('margin-left','-'+link_width+'px');
     $(this).children('.vocab_edit_link').fadeIn();
   }
   var vocab_out = function() {
